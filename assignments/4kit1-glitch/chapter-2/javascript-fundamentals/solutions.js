85c50853b4897eda3723453764737a41d56533a6
Kit <4jrkit1@gmail.com>
2026-03-05 23:14:21 +0100

    feat : added logical operators

--- assignments/4kit1-glitch/chapter-2/javascript-fundamentals/solutions.js
+++ assignments/4kit1-glitch/chapter-2/javascript-fundamentals/solutions.js
@@ -26,6 +26,10 @@ const product = 10 * 5;
 const quotient = 10 / 5;
 const remainder = 10 % 3;
 
-
+// Comparison
+console.log(5 == "5");
+console.log(0 === false);
+console.log(5 > 3);   
+console.log(3 >= 3);      
 
 
